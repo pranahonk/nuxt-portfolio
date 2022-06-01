d1a424e817ea477083871769bd33aa4630404f56
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2022-06-01 10:54:06 +0700

    add animation vue-typed-js

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,9 +5,15 @@
         <section class="hero container max-w-screen-lg mx-auto flex justify-center">
           <img class="grid place-items-center rounded-full h-56 w-56" src='~/assets/image/avatar-transparent.png' alt='Avatars'>
         </section>
-        <span class="font-normal dark:text-gray-300"> Hey, I am </span>{{ $config.devName}}</h2>
-      <h3 class="font-bold text-primary dark:text-primary text-2xl md:text-3xl lg:text-4xl">a {{ $config.devRole}}</h3>
-      <p class="text-lg lg:text-xl font-semibold">{{ $config.devDescription}}</p>
+        <span class="font-normal dark:text-gray-300">
+          Hey, I am </span>{{ $config.devName}}
+      </h2>
+      <h3 class="font-bold text-primary dark:text-primary text-2xl md:text-3xl lg:text-4xl">
+        a happy {{ $config.devRole}}
+      </h3>
+      <p class="text-lg lg:text-xl font-semibold">
+        {{ $config.devDescription}}
+      </p>
       <div class="mt-5">
         <TechStack/>
       </div>
---
+++ plugins/vue-typed-js.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import VueTypedJs from 'vue-typed-js'
+
+console.log('masuk sini')
+
+Vue.use(VueTypedJs)
