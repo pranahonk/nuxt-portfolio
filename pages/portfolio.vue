d32ab00ba337df4e27c09c40d4edb381231a71da
prana.wijaya <prana.wijaya@myboost.co>
2023-06-05 01:20:11 +0700

    fixing images broken

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,91 +1,18 @@
 <script>
 export default {
   async asyncData({$notion, $config: { notionPortfolioPageId }}) {
-    const blockMap = await $notion.getPageBlocks(notionPortfolioPageId);
-    // const portfolios = blockMap.filter((page) => page.public);
+    const blockMap = await $notion.getPageBlocks(notionPortfolioPageId)
     return {blockMap}
   },
   head: {
     title: "Portfolio"
-  },
-  props: {
-    title: {
-      type: String,
-      default: 'Blogs',
-    },
-    posts: {
-      type: Array,
-      default() {
-        return []
-      },
-    },
-  },
-  methods: {
-    formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' }
-      return new Date(date).toLocaleDateString('en', options)
-    },
-    getImage(thumbnail){
-      if(thumbnail){
-        return thumbnail[0]?.url
-      }
-      else{
-        return "https://via.placeholder.com/1000x1000?text=Placeholder"
-      }
-    }
   },
 }
 </script>
 
 
 <template>
-  <div class="mt-16">
-    <div class="wrapper-small my-5">
-      <div
-        v-for="post of portfolios"
-        :key="post.slug"
-        class="project-card md:flex mt-10"
-      >
-        <div class="img max-w-lg md:max-w-sm mx-auto m-2">
-          <nuxt-link :to="`/posts/${post.slug}`">
-            <img
-              :alt="post.title"
-              :src="getImage(post.thumbnail)"
-              class="rounded-xl h-44 w-96 object-cover object-center"
-            />
-          </nuxt-link>
-        </div>
-        <div class="flex flex-col justify-between max-w-lg mx-auto">
-          <div class="txt md:px-5 lg:px-0">
-            <nuxt-link :to="`/posts/${post.slug}`">
-              <h2
-                class="text-xl font-semibold text-gray-800 dark:text-gray-100"
-              >
-                {{ post.title }}
-              </h2>
-            </nuxt-link>
-            <div class="flex flex-col justify-between max-w-lg mx-auto"></div>
-            <span
-              v-for="tag of post.tags"
-              :key="tag"
-              class="font-semibold text-gray-600 bg-opacity-25 dark:bg-opacity-40 dark:text-gray-300 text-sm rounded bg-gray-200 dark:bg-primary mr-1 px-1"
-            >
-              #{{ tag }}
-            </span>
-            <p class="text-base text-gray-700 dark:text-gray-200 my-1">
-              {{ post.description }}
-            </p>
-            <nuxt-link
-              :to="`/posts/${post.slug}`"
-              class="text-base font-semibold text-gray-700 dark:text-gray-200 my-3 hover:underline"
-            >
-              Read more >
-            </nuxt-link>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <NotionRenderer :block-map="blockMap" class='wrapper-small my-5' full-page prism/>
 </template>
 
 
